6686b1dcdf8c5b3ddb1fd0bd0e1b5414b65afb49
Will Szumski <will@cowboycoders.org>
2013-05-17 16:35:43 +0100

    few extra comments

--- jformica_java/src/org/cowboycoders/ant/examples/BasicHeartRateMonitor.java
+++ jformica_java/src/org/cowboycoders/ant/examples/BasicHeartRateMonitor.java
@@ -13,9 +13,20 @@ import org.cowboycoders.ant.messages.SlaveChannelType;
 import org.cowboycoders.ant.messages.data.BroadcastDataMessage;
 
 class Listener implements BroadcastListener<BroadcastDataMessage> {
-
+	
+	/*
+	 * One registered with a channel, this is called every time a broadcast message is received
+	 * on that channel.
+	 * 
+	 * (non-Javadoc)
+	 * @see org.cowboycoders.ant.events.BroadcastListener#receiveMessage(java.lang.Object)
+	 */
 	@Override
 	public void receiveMessage(BroadcastDataMessage message) {
+		/*
+		 * getData() returns the 8 byte payload. The current heart rate
+		 * is contained in the last byte.
+		 */
 		System.out.println("Heart rate: " + message.getData()[7]);
 	}
 
@@ -23,20 +34,67 @@ class Listener implements BroadcastListener<BroadcastDataMessage> {
 
 public class BasicHeartRateMonitor {
 	
-	public static final Level LOG_LEVEL = Level.SEVERE;
+	/*
+	 * See ANT+ data sheet for explanation
+	 */
+	private static final int HRM_CHANNEL_PERIOD = 8070;
+	
+	/*
+	 * See ANT+ data sheet for explanation
+	 */
+	private static final int HRM_CHANNEL_FREQ = 57;
+	
+	/*
+	 * This should match the device you are connecting with.
+	 * Some devices are put into pairing mode (which sets this bit).
+	 * 
+	 * Note: Many ANT+ sport devices do not set this bit (eg. HRM strap).
+	 * 
+	 * See ANT+ docs.
+	 */
+	private static final boolean HRM_PAIRING_FLAG = false;
 
-	public static void main(String[] args) throws InterruptedException {
-		// first usb ant-stick
-		AntTransceiver antchip = new AntTransceiver(0);
-		
+	/*
+	 * slave:
+	 * 0: receive only
+	 * 1: tx/rx
+	 */
+	private static final int HRM_TRANSMISSION_TYPE = 0;
+	
+	/*
+	 * device type for ANT+ heart rate monitor 
+	 */
+	private static final int HRM_DEVICE_TYPE = 120;
+	
+	/*
+	 * You should make a note of the device id and use it in preference to the wild card
+	 * to pair to a specific device.
+	 * 
+	 * 0: wild card, matches all device ids
+	 * any other number: match specific device id
+	 */
+	private static final int HRM_DEVICE_ID = 0;
+	
+	
+	public static final Level LOG_LEVEL = Level.SEVERE;
+	
+	public static void setupLogging() {
 		// set logging level
 	    AntTransceiver.LOGGER.setLevel(LOG_LEVEL);
 	    ConsoleHandler handler = new ConsoleHandler();
 	    // PUBLISH this level
 	    handler.setLevel(LOG_LEVEL);
 	    AntTransceiver.LOGGER.addHandler(handler);
+	}
+	
+
+	public static void main(String[] args) throws InterruptedException {
 		
-		antchip.start();
+		// optional: enable console logging with Level = LOG_LEVEL
+		setupLogging();
+		
+		// first usb ant-stick
+		AntTransceiver antchip = new AntTransceiver(0);
 		
 		Node node = new Node(antchip);
 
@@ -59,13 +117,14 @@ public class BasicHeartRateMonitor {
 		// use ant network key "N:ANT+" 
 		channel.assign("N:ANT+", channelType);
 		
+		// registers an instance of our callback with the channel
 		channel.registerRxListener(new Listener(), BroadcastDataMessage.class);
 
-		channel.setId(0, 120, 0, false);
+		channel.setId(HRM_DEVICE_ID, HRM_DEVICE_TYPE, HRM_TRANSMISSION_TYPE, HRM_PAIRING_FLAG);
 
-		channel.setFrequency(57);
+		channel.setFrequency(HRM_CHANNEL_FREQ);
 
-		channel.setPeriod(8070);
+		channel.setPeriod(HRM_CHANNEL_PERIOD);
 		
 		channel.setSearchTimeout(Channel.SEARCH_TIMEOUT_NEVER);
 
